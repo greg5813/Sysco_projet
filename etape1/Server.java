d570ea1781bfbb333badd5a90f9bd1351f6d8ecf
gregoire.martini@free.fr <gregoire.martini@free.fr>
2015-12-14 11:57:02 +0100

    adebut implementation synchro locksjout fichiers projet

--- etape1/Server.java
+++ etape1/Server.java
@@ -1,6 +1,8 @@
 import java.rmi.RemoteException;
 
 public class Server implements Server_itf{
+	
+	int id = 0;
 
 	@Override
 	public int lookup(String name) throws RemoteException {
@@ -17,7 +19,7 @@ public class Server implements Server_itf{
 	@Override
 	public int create(Object o) throws RemoteException {
 		// TODO Auto-generated method stub
-		return 0;
+		return id++;
 	}
 
 	@Override
--- etape1/SharedObject.java
+++ etape1/SharedObject.java
@@ -9,19 +9,45 @@ public class SharedObject implements Serializable, SharedObject_itf {
 					// (4) WLT: write lock taken
 					// (5) RLT_WLC: read lock taken and write lock cached
 	
-	public Sentence obj;
+	public Object obj;
 	public int id;
+	
+	public SharedObject(Object o) {
+		this.obj = o;
+	}
 	
 	// invoked by the user program on the client node
 	public void lock_read() {
+		Client.lock_read(id);
+		switch (lock) {
+		case 0: 		// NL: no local lock
+			lock = 3; 	// RLT: read lock taken
+			break;
+		case 1: 		// RLC: read lock cached (not taken)
+			lock = 3; 	// RLT: read lock taken
+			break;	
+		case 2: 		// WLC: write lock cached
+			lock = 5; 	// RLT_WLC: read lock taken and write lock cached
+			break;	
+		}
 	}
 
 	// invoked by the user program on the client node
 	public void lock_write() {
+		Client.lock_write(id);
+		lock = 4;
 	}
 
 	// invoked by the user program on the client node
 	public synchronized void unlock() {
+		switch (lock) {
+		case 3: 		// RLT: read lock taken
+			lock = 1; 	// RLC: read lock cached (not taken)
+			break;
+		case 4: 		// WLT: write lock taken
+			lock = 2; 	// WLC: write lock cached
+			break;	
+		}
 	}
 
 
